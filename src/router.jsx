25bd470145ad07247ce2793bdc735175803b75a7
TenkaTosic <mcpcvalentinoplayz@gmail.com>
2026-07-27 11:39:23 +0300

    Private routes for profile and add project ideas

--- src/router.jsx
+++ src/router.jsx
@@ -1,19 +1,18 @@
 import { createBrowserRouter } from "react-router-dom";
-import App from "./App";
 import Supervisors from "./components/Supervisors";
 import Signup from "./components/Signup";
 import Signin from "./components/Signin";
 import ProjectIdeas from "./components/Project_ideas";
 import CreateProfile from "./components/Profile";
 import AddProject from "./components/Add_Project_Idea";
+import PrivateRoute from "./components/PrivateRoute";
 
 
 export const router = createBrowserRouter([
-    {path: "/", element: <Supervisors />},
-    {path: "/supervisor-finder", element: <Supervisors />},
-    {path: "/signup", element: <Signup />},
-    {path: "/signin", element: <Signin />},
-    {path: "/project-ideas", element: <ProjectIdeas />},
-    {path: "/profile", element: <CreateProfile />},
-    {path: "/add-project-idea", element: <AddProject />},
+    {path: "/", element:<Supervisors />},
+    {path: "/signup", element:<Signup />},
+    {path: "/signin", element:<Signin />},
+    {path: "/project-ideas", element:<ProjectIdeas /> },
+    {path: "/profile", element: <PrivateRoute> <CreateProfile/> </PrivateRoute>},
+    {path: "/add-project-idea", element: <PrivateRoute><AddProject /></PrivateRoute> },
 ]);
